b80d624307ecab25e2a0f47f81a4247919c768fe
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-08-04 19:51:01 -0300

    req 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,39 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const result = await fs.readFile('./talker.json', 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+  });
+  console.log(result);
+  const talkers = JSON.parse(result);
+
+  return res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const result = await fs.readFile('./talker.json', 'utf8');
+
+  const talkers = JSON.parse(result);
+
+  const talkerFiltered = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!talkerFiltered) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  console.log(talkerFiltered);
+
+  return res.status(200).json(talkerFiltered);
 });
 
 app.listen(PORT, () => {
