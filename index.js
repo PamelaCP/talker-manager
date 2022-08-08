39db18031b96c25252b89d9189d0e48442e70946
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-08-08 18:13:43 -0300

    req 3

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -25,6 +30,11 @@ app.get('/talker', async (req, res) => {
 
   return res.status(200).json(talkers);
 });
+
+const generateToken = () => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return token;
+};
 
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
@@ -46,6 +56,8 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(talkerFiltered);
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.post('/login', (req, res) => {
+  const token1 = generateToken();
+  console.log(token1);
+  return res.status(200).json({ token: token1 });
 });
