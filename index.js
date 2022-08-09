005b114ab6279bb722fee8a86067eada6cc652ce
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-08-09 14:31:57 -0300

    refazendo req 5

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
 const crypto = require('crypto');
-const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,103 +9,25 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 
-// funcão auxiliar de validação:
-
-function tokenValidator(token) {
-  if (!token) {
-    return { message: 'Token não encontrado' };
-  } if (token.length !== 16) {
-    return { message: 'Token inválido' };
-  }
-  return false;
-} 
-
- function getTalkers() {
- const talkers = fs.readFile('./talker.json', 'utf-8');
-//  await fs.readFile(
-//     path.join(__dirname, '', 'talker.json'), 'utf8',
-//   );
-  return JSON.parse(talkers);
-}
-
-function createNewTalker(name, age, talk) {
-  return { name, age, talk };
-}
-
-function nameValidator(name) {
-  if (!name) {
-    return { message: 'O campo "name" é obrigatório' };
-  } if (name.length < 3) {
-    return { message: 'O "name" deve ter pelo menos 3 caracteres' };
-  }
-}
-
-function ageValidator(age) {
-  if (!age) {
-    return { message: 'O campo "age" é obrigatório' };
-  } if (age < 18) {
-    return { message: 'A pessoa palestrante deve ser maior de idade' };
-  }
-}
-
-function talkWatchedAtValidator(talk) {
-  const dateRegex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
-  if (!talk.watchedAt) {
-    return { message: 'O campo "watchedAt" é obrigatório' };
-  } if (!dateRegex.test(talk.watchedAt)) {
-    return { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
-  }
-}
-
-function talkRateValidator(talk) {
-  if (talk.rate < 1 || talk.rate > 5) {
-  return { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
-  } if (!talk.rate) {
-    return { message: 'O campo "rate" é obrigatório' };
-  }
-}
-
-function talkValidator(talk) {
-  if (!talk) {
-    return { message: 'O campo "talk" é obrigatório' };
-  }
-  const watchedAt = talkWatchedAtValidator(talk);
-  const rate = talkRateValidator(talk);
-  if (watchedAt) {
-    return (watchedAt);
-  } if (rate) {
-    return (rate);
-  }
-}
-
-function talkerValidator(talker) {
-  const name = nameValidator(talker.name);
-  const age = ageValidator(talker.age);
-  const talk = talkValidator(talker.talk);
-
-  if (name) {
-    return name;
-  } if (age) {
-    return age;
-  } if (talk) {
-    return talk;
-  }
-  return false;
-}
+// funcão auxiliar de validação 
 
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', async (req, res) => {
-  const result = await fs.readFile('./talker.json', 'utf-8');
-    // if (err) {
-    //   console.log(err);
-    //   return err;
-    // }
-  // });
+  const result = await fs.readFile('./talker.json', 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+  });
   console.log(result);
   const talkers = JSON.parse(result);
 
@@ -150,17 +71,45 @@ app.post('/login', (req, res) => {
   } if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+
   const token1 = generateToken();
   console.log(token1);
   return res.status(200).json({ token: token1 });
 });
-app.post('/talker', async (req, res) => {
-  if (tokenValidator(req.headers.authorization)) {
-    return res.status(401).send(tokenValidator(req.headers.authorization));
-  }
 
+function tokenValidator(res, req, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  } if (token.length !== 16) { 
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+} 
+
+function nameValidator(res, req, next) {
+  const { name } = req.body.name;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  } if (name.length < 3) {
+   return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function ageValidator(res, req, next) {
+  const { age } = req.body.age;
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  } if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+
+app.post('/talker', tokenValidator, nameValidator, ageValidator, async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
   const { name, age, talk } = req.body;
-  const talkers = getTalkers();
   const newTalker = createNewTalker(name, age, talk);
   if (talkerValidator(newTalker)) {
     return res.status(400).send(talkerValidator(newTalker));
@@ -168,13 +117,9 @@ app.post('/talker', async (req, res) => {
   newTalker.id = talkers.length + 1;
   const newTalkers = talkers;
   newTalkers.push(newTalker);
- await fs.writeFile(
+  fs.writeFileSync(
     path.join(__dirname, '', 'talker.json'), JSON.stringify(newTalkers), 
   );
 
   return res.status(201).send(newTalker);
-});
-
-app.listen(PORT, () => {
-  console.log('Online');
 });
